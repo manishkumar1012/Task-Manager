183c3c76bfe02538507d4d9c84ecc6e2bc64776c
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-02-21 17:26:09 +0530

    Updated User by Id

--- src/index.js
+++ src/index.js
@@ -43,18 +43,28 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
-// app.patch('/users/:id', async (req, res) => {
-//     try {
-//         const user = await User.findByIdAndUpdate(req.params.id, { new: true, runValidators: true });
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    });
+    
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Invalid Operation!' });
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true },);
         
-//         if (!user) {
-//             res.status(404).send();
-//         }
-//         res.send(user);
-//     } catch(e) {
-//         res.status(400).send(e);
-//     }
-// })
+        if (!user) {
+            res.status(404).send();
+        }
+        res.send(user);
+    } catch(e) {
+        res.status(400).send(e);
+    }
+})
 
 app.post("/tasks", async (req, res) => {
   const newTask = new Task(req.body);
