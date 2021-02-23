7eb82ebfe8b67f4994c9f7a1dd5f359b4b3979ce
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-02-23 19:20:22 +0530

    Generated jwt

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -15,6 +16,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(value) {
@@ -34,7 +36,35 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens: [{
+    token: {
+      type: String,
+      required: true,
+    }
+  }],
 });
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = await jwt.sign({ _id: user._id.toString() }, "secretissecret");
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new error('Unable to login!');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new error('Unable to login!');
+  }
+
+  return user;
+};
 
 userSchema.pre("save", async function(next) {
   const user = this;
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,12 +3,23 @@ const router = new express.Router();
 const User = require("../models/user");
 
 router.post("/users", async (req, res) => {
-  const newUser = new User(req.body);
+  const user = new User(req.body);
 
   try {
-    await newUser.save();
-    res.status(201).send(newUser);
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch(e) {
     res.status(400).send(e);
   }
 });
